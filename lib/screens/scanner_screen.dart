e6238ffcfb8cfb68a43ed4572e269482cdcccbc2
Leider maza taboada <mtzzzleider580@gmail.com>
2026-06-24 11:33:15 -0500

    Camara integrada con MobileScanner

--- lib/screens/scanner_screen.dart
+++ lib/screens/scanner_screen.dart
@@ -1,7 +1,7 @@
 // Importa los componentes visuales de Flutter
 import 'package:flutter/material.dart';
 
-// Importa el paquete del escáner QR
+// Importa el paquete para usar la cámara y leer códigos QR
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 // Pantalla del escáner
@@ -18,14 +18,14 @@ class ScannerScreen extends StatelessWidget {
         centerTitle: true,
       ),
 
-      // Contenido principal
+      // Cuerpo de la pantalla
       body: Column(
         children: [
-          // Texto informativo
+          // Título informativo
           const Padding(
             padding: EdgeInsets.all(10),
             child: Text(
-              "Prueba de integración del escáner",
+              "Apunta la cámara hacia un código QR",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -33,17 +33,32 @@ class ScannerScreen extends StatelessWidget {
             ),
           ),
 
-          // Área donde más adelante veremos la cámara
+          // La cámara ocupará el resto de la pantalla
           Expanded(
-            child: Container(
-              color: Colors.black12,
+            child: MobileScanner(
+              // Se ejecuta cuando detecta un código
+              onDetect: (capture) {
+                // Obtiene los códigos detectados
+                final List<Barcode> barcodes = capture.barcodes;
 
-              child: const Center(
-                child: Text(
-                  "Aquí aparecerá la cámara",
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
+                // Recorre los códigos encontrados
+                for (final barcode in barcodes) {
+                  // Verifica que tenga información
+                  if (barcode.rawValue != null) {
+                    // Muestra el contenido encontrado
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Código detectado: ${barcode.rawValue}',
+                        ),
+                      ),
+                    );
+
+                    // Sale del ciclo para evitar múltiples mensajes
+                    break;
+                  }
+                }
+              },
             ),
           ),
         ],
